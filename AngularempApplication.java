365a21c46a421ffb6c7a8f8dde9790e6712ab933
Raji23 <rajalakshmi.lingam96@gmail.com>
2018-02-15 18:27:02 +0530

    Add files via upload

---
+++ AngularempApplication.java
@@ -0,0 +1,21 @@
+package com.kgisl.angulardemo.angularemp;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AngularempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularempApplication.class, args);
+	}
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+		registration.addUrlMappings("/console/*");
+		return registration;
+	}
+}
